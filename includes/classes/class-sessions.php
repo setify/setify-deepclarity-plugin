0e0523f97d4e8efd8bcaccb93b9bf98b97535faf
Piwi <mail@setify.de>
2025-12-20 20:47:18 +0100

    style: Verbessere den Code-Stil in der Sessions-Klasse durch Anpassungen der Formatierung und Konsistenz

--- includes/classes/class-sessions.php
+++ includes/classes/class-sessions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Sessions class for custom queries
  *
@@ -8,7 +9,7 @@
 namespace DeepClarity;
 
 // Prevent direct access
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
     exit;
 }
 
@@ -17,7 +18,8 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * Handles session-related queries and functionality.
  */
-class Sessions {
+class Sessions
+{
 
     /**
      * Post type name
@@ -31,21 +33,23 @@ class Sessions {
      *
      * @var string
      */
-    const ACF_CLIENT_FIELD = 'client';
+    const ACF_CLIENT_FIELD = 'session_client';
 
     /**
      * Constructor
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->register_query_filters();
     }
 
     /**
      * Register custom query filters for Unlimited Elements
      */
-    private function register_query_filters() {
+    private function register_query_filters()
+    {
         // Unlimited Elements custom query filter
-        add_filter( 'sessions_current_client', array( $this, 'query_sessions_current_client' ), 10, 2 );
+        add_filter('sessions_current_client', array($this, 'query_sessions_current_client'), 10, 2);
     }
 
     /**
@@ -57,11 +61,12 @@ class Sessions {
      * @param array $widget_data Widget settings.
      * @return array Modified query arguments.
      */
-    public function query_sessions_current_client( $args, $widget_data ) {
+    public function query_sessions_current_client($args, $widget_data)
+    {
         // Get current post ID
         $current_post_id = get_the_ID();
 
-        if ( ! $current_post_id ) {
+        if (! $current_post_id) {
             return $args;
         }
 
@@ -86,10 +91,10 @@ class Sessions {
         );
 
         // Merge with existing meta_query if present
-        if ( isset( $args['meta_query'] ) && is_array( $args['meta_query'] ) ) {
+        if (isset($args['meta_query']) && is_array($args['meta_query'])) {
             $args['meta_query'][] = $meta_query;
         } else {
-            $args['meta_query'] = array( $meta_query );
+            $args['meta_query'] = array($meta_query);
         }
 
         return $args;
@@ -102,7 +107,8 @@ class Sessions {
      * @param array $args      Additional query arguments.
      * @return \WP_Query
      */
-    public static function get_sessions_for_client( $client_id, $args = array() ) {
+    public static function get_sessions_for_client($client_id, $args = array())
+    {
         $default_args = array(
             'post_type'      => self::POST_TYPE,
             'posts_per_page' => -1,
@@ -122,8 +128,8 @@ class Sessions {
             ),
         );
 
-        $query_args = wp_parse_args( $args, $default_args );
+        $query_args = wp_parse_args($args, $default_args);
 
-        return new \WP_Query( $query_args );
+        return new \WP_Query($query_args);
     }
 }
